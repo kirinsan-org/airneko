4fc220d7774156e80e36e8d0a0ea9d0a1af2d74d
Jun Tanaka <junpluse@gmail.com>
2016-06-11 17:07:25 +0900

    class Cat

---
+++ iOS/Airneko/Cat.swift
@@ -0,0 +1,79 @@
+//
+//  Cat.swift
+//  Airneko
+//
+//  Created by Jun Tanaka on 6/11/16.
+//  Copyright © 2016 eje Inc. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import ReactiveCocoa
+
+final class Cat {
+	enum State: String {
+		case Idle = "" // 初期状態
+		case Sleeping = "sleeping" // 寝ている
+		case Angry = "angry" // 怒っている
+		case Playful = "playful" // じゃれている
+		case Yawn = "yawn" // あくびしている
+		case Delightful = "delightful" // よろこんでいる
+		case Eating = "eating" // eating
+		case Unko = "unko" // unko
+	}
+
+	enum Place: Equatable {
+		case User(String) // 誰かのところ
+		case Some // 誰かのところではないどこか
+	}
+
+	let name = MutableProperty<String>("")
+	let state = MutableProperty<State>(.Idle)
+	let place = MutableProperty<Place>(.Some)
+	let hungry = MutableProperty<Float>(0)
+	let unko = MutableProperty<Float>(0)
+
+	let firebaseReference: FIRDatabaseReference
+
+	static var defaultFirebaseReference: FIRDatabaseReference {
+		return FIRDatabase.database().referenceWithPath("family/sample/neko/sampleneko")
+	}
+
+	init(firebaseReference ref: FIRDatabaseReference = defaultFirebaseReference) {
+		firebaseReference = ref
+
+		ref.observeEventType(FIRDataEventType.Value, withBlock: { [weak self] snapshot in
+			guard let dictionary = snapshot.value as? [String : AnyObject] else {
+				fatalError("invalid response")
+			}
+			if let value = dictionary["state"] as? String, let state = State(rawValue: value) {
+				self?.state.value = state
+			}
+			if let value = dictionary["name"] as? String {
+				self?.name.value = value
+			}
+			if let value = dictionary["place"] as? String {
+				self?.place.value = .User(value)
+			} else {
+				self?.place.value = .Some
+			}
+			if let value = dictionary["hungry"] as? Float {
+				self?.hungry.value = value
+			}
+			if let value = dictionary["unko"] as? Float {
+				self?.unko.value = value
+			}
+		})
+	}
+}
+
+func ==(lhs: Cat.Place, rhs: Cat.Place) -> Bool {
+	switch (lhs, rhs) {
+	case (.User(let user1), .User(let user2)):
+		return user1 == user2
+	case (.Some, .Some):
+		return true
+	default:
+		return false
+	}
+}
