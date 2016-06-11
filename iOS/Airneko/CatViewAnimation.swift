935147a52dec02b1c46044c99d54b72a824e0b51
星野恵瑠 <shonetesmith@gmail.com>
2016-06-11 22:16:02 +0900

    Add CatView extension for animation

---
+++ iOS/Airneko/CatViewAnimation.swift
@@ -0,0 +1,15 @@
+//
+//  CatViewAnimation.swift
+//  Airneko
+//
+//  Created by 史翔新 on 2016/06/11.
+//  Copyright © 2016年 eje Inc. All rights reserved.
+//
+
+import UIKit
+
+extension CatView {
+	
+	
+	
+}
