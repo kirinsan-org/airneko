3ae115e4f8a96ff17fb3b4b59a7d83a1a6d3e610
星野恵瑠 <shonetesmith@gmail.com>
2016-06-11 23:07:40 +0900

    Add jump animation

--- iOS/Airneko/CatViewAnimation.swift
+++ iOS/Airneko/CatViewAnimation.swift
@@ -10,6 +10,26 @@ import UIKit
 
 extension CatView {
 	
-	
+	func jump() {
+		
+		UIView.animateKeyframesWithDuration(0.4, delay: 0, options: [], animations: {
+			UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.25) {
+				self.imageView.frame.origin.y -= 70
+			}
+			UIView.addKeyframeWithRelativeStartTime(0.25, relativeDuration: 0.25, animations: {
+				self.layoutSubviews()
+			})
+			UIView.addKeyframeWithRelativeStartTime(0.5, relativeDuration: 0.25, animations: {
+				self.imageView.frame.origin.y += 40
+				self.imageView.frame.origin.x -= 10
+				self.imageView.frame.size.height -= 40
+				self.imageView.frame.size.width += 20
+			})
+			UIView.addKeyframeWithRelativeStartTime(0.75, relativeDuration: 0.25, animations: { 
+				self.layoutSubviews()
+			})
+		}, completion: nil)
+		
+	}
 	
 }
