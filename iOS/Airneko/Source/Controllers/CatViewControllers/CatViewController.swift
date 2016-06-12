c7aa59a21119aac39738c3c10bce0dfeda83e950
星野恵瑠 <shonetesmith@gmail.com>
2016-06-12 13:06:12 +0900

    Observe shake motion

--- iOS/Airneko/Source/Controllers/CatViewControllers/CatViewController.swift
+++ iOS/Airneko/Source/Controllers/CatViewControllers/CatViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import ReactiveCocoa
+import CoreMotion
 
 final class CatViewController: UIViewController {
 	
 	private let catModel: Cat
 	private let catView: CatView
+	private let shakeManager: ShakeManager
 	
 	var catState: Cat.State {
 		return catModel.state.value
@@ -21,12 +23,18 @@ final class CatViewController: UIViewController {
 	init(cat: Cat? = nil, catView: CatView? = nil, nibName nibNameOrNil: String? = nil, bundle nibBundleOrNil: NSBundle? = nil) {
 		self.catModel = cat ?? Cat()
 		self.catView = catView ?? CatView()
+		self.shakeManager = ShakeManager()
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 		self.catView.delegate = self
+		self.shakeManager.delegate = self
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+	
+	deinit {
+		shakeManager.endObservingShakeMotion()
 	}
 	
 	override func loadView() {
@@ -43,15 +51,25 @@ final class CatViewController: UIViewController {
 		button.setImage(buttonImage, forState: .Normal)
 		button.addTarget(self, action: #selector(CatViewController.addEsa), forControlEvents: .TouchUpInside)
 		catView.itemButton = button
+		
+		shakeManager.startObservingShakeMotion()
 	}
 
 	override func preferredStatusBarStyle() -> UIStatusBarStyle {
 		return .LightContent
 	}
 
-//	override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//		catView.doAnimation()
-//	}
+}
+
+extension CatViewController: ShakeManagerDelegate {
+	
+	func didStartShake() {
+		print("started")
+	}
+	
+	func didFinishShake() {
+		print("finished")
+	}
 	
 }
 
