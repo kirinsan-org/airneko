1cad6e6e0c2790b316a4b8e1cd29903ebbe79ff1
Jun Tanaka <junpluse@gmail.com>
2016-06-12 11:50:22 +0900

    Push Notifications w/ nyaa sounds!

--- iOS/Airneko/AppDelegate.swift
+++ iOS/Airneko/AppDelegate.swift
@@ -8,29 +8,47 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		registerForPushNotifications(application)
+
 		FIRApp.configure()
 
 		window = UIWindow(frame: UIScreen.mainScreen().bounds)
 		window?.rootViewController = ViewController(nibName: nil, bundle: nil)
 		window?.makeKeyAndVisible()
 
+		let delay = 3.0 * Double(NSEC_PER_SEC)
+		let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+		dispatch_after(time, dispatch_get_main_queue(), {
+			NyaaPlayer.sharedInstance.play("nyaa1.wav")
+		})
+
 		return true
+	}
+	
+
+	func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+		// If you are receiving a notification message while your app is in the background,
+		// this callback will not be fired till the user taps on the notification launching the application.
+		// TODO: Handle data of notification
+
+		// play nyaa sound in payload
+		NyaaPlayer.sharedInstance.play(notificationPayload: userInfo)
+
+		// Print full message
+		print("%@", userInfo)
 	}
 
 	func applicationWillResignActive(application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-	}
-
-	func applicationDidEnterBackground(application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 	}
 
 	func applicationWillEnterForeground(application: UIApplication) {
@@ -43,5 +61,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationWillTerminate(application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+	}
+
+	func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+		if notificationSettings.types != .None {
+			application.registerForRemoteNotifications()
+		}
+	}
+
+	func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+		let tokenChars = UnsafePointer<CChar>(deviceToken.bytes)
+		var tokenString = ""
+
+		for i in 0..<deviceToken.length {
+			tokenString += String(format: "%02.2hhx", arguments: [tokenChars[i]])
+		}
+
+		let instanceID = FIRInstanceID.instanceID()
+		instanceID.setAPNSToken(deviceToken, type: FIRInstanceIDAPNSTokenType.Unknown)
+
+		// store Firebase instance token to database
+		if let token = instanceID.token() {
+			let ref = FIRDatabase.database().referenceWithPath("family/sample/token/\(token)")
+			ref.setValue(true)
+		}
+	}
+}
+
+extension AppDelegate {
+	func registerForPushNotifications(application: UIApplication) {
+		let notificationSettings = UIUserNotificationSettings(forTypes: [.Badge, .Sound, .Alert], categories: nil)
+		application.registerUserNotificationSettings(notificationSettings)
 	}
 }
